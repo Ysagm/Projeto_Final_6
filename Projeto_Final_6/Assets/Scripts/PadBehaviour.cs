d0a1ad3a6468680112558d5fc142d18194e6a4b0
Ysadora G. M <ysagm@icloud.com>
2023-09-26 16:24:12 -0300

    n sei oq estou fazendo

---
+++ Projeto_Final_6/Assets/Scripts/PadBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadBehaviour : MonoBehaviour {
+
+	public float jumpForce = 10f;
+
+	void OnCollisionEnter(Collision other){
+		if(other.relativeVelocity.y <= 0){
+			Rigidbody rb = other.collider.GetComponent<Rigidbody>();
+			if( rb != null) {
+				rb.velocity = new Vector2(0,jumpForce);
+			}
+		}
+	} 
+}
---
+++ Projeto_Final_6/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour 
+{
+
+	Rigidbody rb;
+	float movement = 0f;
+	public float movementSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement = Input.GetAxis("Horizontal") * movementSpeed;
+	}
+
+	void FixedUpdate(){
+		rb.velocity = new Vector2(movement, rb.velocity.y);
+	}
+}
